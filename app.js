ba7ebeb2df9ce37aa33d3041a3f9852860ddf61b
alex.paclin <alex.paclin@gmail.com>
2018-08-17 23:31:21 +0500

    085 Updating the Percentages_ Controller

--- app.js
+++ app.js
@@ -224,6 +224,17 @@ var controller = (function (budgetCtrl, UICtrl) {
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
     };
 
+    var updatePercentages = function () {
+        // 1. Calculate percentages
+
+
+        // 2. Read percentages from the budget controller
+
+
+        // 3. Update the UI with the new percentages
+
+    };
+
     var updateBudget = function () {
         // 1. Calculate the budget
         budgetCtrl.calculateBudget();
@@ -254,6 +265,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
         }
     };
 
@@ -275,6 +289,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
             // 3. Update and show the new budget
             updateBudget();
+
+            // 4. Calculate and update percentages
+            updatePercentages();
         }        
     };
 
